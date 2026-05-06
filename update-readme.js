ebfb369322086481e13ac818299b6253f7b628ac
Fernando de los Ríos Sánchez <fernando.delosrios@sailpoint.com>
2026-05-06 22:44:37 +0200

    chore: remove deprecated README update scripts

--- update-readme.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let readme = fs.readFileSync('README.md', 'utf8');
-readme = readme.replace('## Changelog', '## Changelog\n\n- (2026-05-01) Added tests for `generateReport.ts` helpers.');
-fs.writeFileSync('README.md', readme);
