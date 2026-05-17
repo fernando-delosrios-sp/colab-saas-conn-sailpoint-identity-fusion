21f11f207557b3fffaab88f8af611d460ee3a93a
fernando.delosrios-sp <fernando.delosrios@sailpoint.com>
2026-05-17 09:08:01 +0000

    fix: update default PORT to 3000 in log-server.js

--- log-server.js
+++ log-server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const PORT = process.env.PORT || 2389
+const PORT = process.env.PORT || 3000
 const LOG_FILE = process.env.LOG_FILE || 'logs/remote-logs.txt'
 
 // ANSI color codes
